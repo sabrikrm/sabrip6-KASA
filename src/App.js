ea9f969e6e90c75489b233775a8b7c9c5c0dc3ce
sabri karoumi <skaroumi71@gmail.com>
2023-11-01 10:51:12 +0100

    ajout des asset extrait de la maquette

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/app.scss";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import FichLogement from "./pages/logement/FichLogment";
+import NoPage from "./pages/error/NoPage";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
-
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/accueil" />} />
+        <Route path="/accueil" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<FichLogement />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </HashRouter>
   );
-}
+};
 
 export default App;
