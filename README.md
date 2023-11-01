40a6ec1ce28ff8a7d713ed45bd31f3966b182d20
sabri karoumi <skaroumi71@gmail.com>
2023-11-01 12:45:36 +0100

    bientot finis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Sabri karoumi mon projet 6 de la formation developpeur web openclassroom developper une application react en exportant les composant d'une maquette figma
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // ReactDOM pour s'attacher à notre HTML.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//dans la variable root on affiche tout mon application on importe
+
 // le composant parent App.js
 root.render(
   <React.StrictMode>
