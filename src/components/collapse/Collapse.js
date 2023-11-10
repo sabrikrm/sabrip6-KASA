4ec02937468517737f8785e06349769f91e67f85
sabri karoumi <skaroumi71@gmail.com>
2023-11-10 11:33:34 +0100

    animation css des collapse

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 
 const Collapse = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false); 
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleCollapse = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(!isOpen); // Change l'état isOpen entre true et false
   };
 
   return (
     <div className="collapse__dropdown__container">
-      <div className="collapse__dropdown__title">
+      <div className="collapse__dropdown__title" onClick={toggleCollapse}>
         <h2>{title}</h2>
-        <p onClick={toggleCollapse}>
+        {/* Ic les classes sont appliquées conditionnellement en fonction de isOpen */}
+        <p>
           <i className={`fa-solid ${isOpen ? 'fa-chevron-up arrow-animate-up' : 'fa-chevron-down arrow-animate-down'}`}></i>
         </p>
       </div>
-      {/* Si le collapse est à TRUE alors il affichera la description */}
+      {/* Le contenu du dropdown est affiché si isOpen est true */}
       <div className={`collapse__dropdown__content ${isOpen ? 'content-visible' : ''}`}>
         {isOpen && <p>{content}</p>}
       </div>
